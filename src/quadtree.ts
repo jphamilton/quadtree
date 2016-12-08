c3ff91d89d07b9e1016aa3bd06f512e5c97ad8db
J.P. Hamilton <instantjp@gmail.com>
2016-12-07 22:08:19 -0600

    initial

--- src/quadtree.ts
+++ src/quadtree.ts
@@ -2,7 +2,7 @@
 
 export default class Quadtree {
 
-    nodes: Quadtree[];    
+    nodes: Quadtree[];
     objects: Rect[];
     height2: number;
     width2: number;
@@ -15,85 +15,85 @@ export default class Quadtree {
     }
 
     insert(rect: Rect) {
-		let i = 0;
-	 	let index;
+        let i = 0;
+        let index;
 
-	 	//if we have subnodes ...
-		if (this.nodes[0]) {
-			index = this.getIndex(rect);
-	 
-		  	if(index !== -1) {
-				this.nodes[index].insert(rect);	 
-			 	return;
-			}
-		}
-	 
-	 	this.objects.push(rect);
-		
-		if(this.objects.length > this.maxObjects && this.level < this.maxLevels) {
-			
-			//split if we don't already have subnodes
-			if(!this.nodes[0]) {
-				this.split();
-			}
-			
-			//add all objects to there corresponding subnodes
-			while(i < this.objects.length) {
-				
-				index = this.getIndex(this.objects[ i ]);
-				
-				if(index !== -1) {					
-					this.nodes[index].insert(this.objects.splice(i, 1)[0]);
-				} else {
-					i = i + 1;
-			 	}
-		 	}
-		}
-	 }
+        //if we have subnodes ...
+        if (this.nodes[0]) {
+            index = this.getIndex(rect);
 
-     retrieve(rect) {
-		let index = this.getIndex(rect);
-		let	result = this.objects;
-			
-		//if we have subnodes ...
-		if (this.nodes[0]) {
-			//if rect fits into a subnode ..
-			if (index !== -1) {
-				result = [...result, ...this.nodes[index].retrieve(rect)];
-			//if rect does not fit into a subnode, check it against all subnodes
-			} else {
-				for (let i=0; i < this.nodes.length; i++) {
-					result = [...result, ...this.nodes[i].retrieve(rect)];
-				}
-			}
-		}
-	 
-		return result;
-	};
+            if (index !== -1) {
+                this.nodes[index].insert(rect);
+                return;
+            }
+        }
+
+        this.objects.push(rect);
+
+        if (this.objects.length > this.maxObjects && this.level < this.maxLevels) {
+
+            //split if we don't already have subnodes
+            if (!this.nodes[0]) {
+                this.split();
+            }
+
+            //add all objects to there corresponding subnodes
+            while (i < this.objects.length) {
+
+                index = this.getIndex(this.objects[i]);
+
+                if (index !== -1) {
+                    this.nodes[index].insert(this.objects.splice(i, 1)[0]);
+                } else {
+                    i = i + 1;
+                }
+            }
+        }
+    }
+
+    retrieve(rect) {
+        let index = this.getIndex(rect);
+        let result = this.objects;
+
+        //if we have subnodes ...
+        if (this.nodes[0]) {
+            //if rect fits into a subnode ..
+            if (index !== -1) {
+                result = [...result, ...this.nodes[index].retrieve(rect)];
+                //if rect does not fit into a subnode, check it against all subnodes
+            } else {
+                for (let i = 0; i < this.nodes.length; i++) {
+                    result = [...result, ...this.nodes[i].retrieve(rect)];
+                }
+            }
+        }
+
+        return result;
+    };
 
     clear() {
-		this.objects = [];
-	 
-		for (let i = 0; i < this.nodes.length; i++) {
-			if(this.nodes[i]) {
-				this.nodes[i].clear();
-		  	}
-		}
+        this.objects = [];
 
-		this.nodes = [];
-	};
+        for (let i = 0; i < this.nodes.length; i++) {
+            if (this.nodes[i]) {
+                this.nodes[i].clear();
+            }
+        }
+
+        this.nodes = [];
+    };
 
     private getIndex(rect: Rect) {
         let index = -1;
         let xmid = this.bounds.x + this.width2;
         let ymid = this.bounds.y + this.height2;
-    
+
         // rect can completely fit within the top quadrants
         let top = (rect.y < ymid && rect.y + rect.height < ymid);
-        
+
         // rect can completely fit within the bottom quadrants
         let bottom = (rect.y > ymid);
-    
+
         // rect can completely fit within the left quadrants
         if (rect.x < xmid && rect.x + rect.width < xmid) {
             if (top) {
@@ -101,8 +101,8 @@ export default class Quadtree {
             } else if (bottom) {
                 index = 2;
             }
-    
-        // rect can completely fit within the right quadrants 
+
+            // rect can completely fit within the right quadrants 
         } else if (rect.x > xmid) {
             if (top) {
                 index = 0;
@@ -110,30 +110,35 @@ export default class Quadtree {
                 index = 3;
             }
         }
-    
+
         return index;
     };
 
     private split() {
         let width = Math.round(this.width2);
-        let height  = Math.round(this.height2);
+        let height = Math.round(this.height2);
         let x = Math.round(this.bounds.x);
-        let y = Math.round(this.bounds.y);  
+        let y = Math.round(this.bounds.y);
 
         let create = (x, y) => {
-            let bounds: Rect = { x: x, y: y, width: width, height: height };
+            let bounds: Rect = {
+                x: x,
+                y: y,
+                width: width,
+                height: height
+            };
             return new Quadtree(bounds, this.maxObjects, this.maxLevels, this.level + 1);
         };
 
         // top right
         this.nodes[0] = create(x + width, y);
-        
+
         //top left
         this.nodes[1] = create(x, y);
 
         //bottom left
         this.nodes[2] = create(x, y + height);
-        
+
         //bottom right
         this.nodes[3] = create(x + width, y + height);
     };
